4f4e860d5be52664386500f8294a849c11bf52c3
Renan Muniz Merlo <renan.mmerlo@gmail.com>
2020-07-22 17:05:24 -0300

    exer10

--- jee-rest-aula/src/main/java/rest/exer9/NegocioException.java
+++ jee-rest-aula/src/main/java/rest/exer9/NegocioException.java
@@ -2,6 +2,11 @@ package rest.exer9;
 
 public class NegocioException extends Exception {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	public NegocioException(String erro) {
 		super(erro);
 	}
--- jee-rest-aula/src/main/java/rest/exer9/RestTituloImp.java
+++ jee-rest-aula/src/main/java/rest/exer9/RestTituloImp.java
@@ -1,7 +1,6 @@
 package rest.exer9;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
